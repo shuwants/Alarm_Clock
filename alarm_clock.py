4de5131f443e33ade4f3c161b3ee4b020b5525f1
Shuji Kato <shuwants@gmail.com>
2020-09-02 19:34:10 +0900

    set 2 functions.

--- alarm_clock.py
+++ alarm_clock.py
@@ -5,5 +5,19 @@ import time
 import os
 
 def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        now = current_time.strftime("%H:%M:%S")
+        date = current_time.strftime("%d/%m/%Y")
+        print("The Set Date is:", date)
+        print(now)
+        if now == set_alarm_timer:
+            print("Time to Wake Up")
+        os.system('say "起きろ！"')
+        break
 
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm (set_alarm_timer)
 
