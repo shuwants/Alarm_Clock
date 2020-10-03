2de46be9a4c7d569abec8f8951c2ccc36ed38955
Shuji Kato <shuwants@gmail.com>
2020-10-03 18:37:03 +0900

    Deleted duplication

--- alarm_clock.py
+++ alarm_clock.py
@@ -33,15 +33,7 @@ time_format=Label(clock, text="Enter time in 24 hour format!",
 addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
 setYourAlarm = Label(clock,text="When to wake you up",fg="blue",relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
 
-# clock.title("Alarm_Clock")
-# clock.geometry("400x200")
-# time_format=Label(clock, text= "Enter time in 24 hour format.",
-#                   fg="red", bg="black", font="Arial").place(x=60,y=120)
-# addTime = Label(clock,text = "Hour Min Set",font=60).place(x = 110)
-# setYourAlarm = Label(clock,text = "WHen to wake you up", fg="blue",relief = "solid",
-#                      font=("Helevetica",7,"bold")).place(x=0,y=29)
 # The Variables we require to set the alarm(inisialization):
-
 hour = StringVar()
 min = StringVar()
 sec = StringVar()
